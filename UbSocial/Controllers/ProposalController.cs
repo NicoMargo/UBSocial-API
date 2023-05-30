0ff971fa3867a5d0de56bcd4872fbcb8f298109f
WaismanM <matiwais7835@gmail.com>
2023-05-30 01:31:11 -0300

    proposal crud
    yes

---
+++ UbSocial/Controllers/ProposalController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UbSocial.Models;
+using UbSocial.Models.Helpers;
+
+namespace UBSocial.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProposalController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ProposalGet()
+        {
+            try
+            {
+                return Ok(DBHelper.callProcedureReader("spProposalGetAll", new Dictionary<string, object> { }));
+            }
+            catch
+            {
+                return StatusCode(500, "Error al obtener la informacion de las propuestas");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ProposalGetById(int id)
+        {
+            try
+            {
+                Dictionary<string, object> args = new Dictionary<string, object> {
+                    {"pId",id}
+                };
+                return Ok(DBHelper.callProcedureReader("spProposalGetById", args));
+            }
+            catch
+            {
+                return StatusCode(500, "Error al obtener la informacion de las propuestas");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            string success = "Error al eliminar la propuesta";
+            try
+            {
+                Dictionary<string, object> args = new Dictionary<string, object> {
+                    {"pId",id}
+                };
+
+                success = DBHelper.CallNonQuery("spProposalDelete", args);
+
+                if (success == "1")
+                {
+                    return Ok();
+                }
+
+                return StatusCode(500, success);
+            }
+            catch
+            {
+                return StatusCode(500, success);
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+
+        public IActionResult Create(Proposal proposal)
+        {
+            string success = "Error al crear la propuesta";
+            try
+            {
+                if (proposal.Title != null && proposal.Description != null)
+                {
+                    Dictionary<string, object> args = new Dictionary<string, object> {
+                         {"pTitle",proposal.Title},
+                         {"pDescription",proposal.Description}
+                    };
+                    success = DBHelper.CallNonQuery("spProposalCreate", args);
+                    if (success == "1")
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(500, success);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return StatusCode(500, success);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Proposal proposal)
+        {
+            string success = "Error al modificar la propuesta";
+            try
+            {
+                if (proposal.Title != null && proposal.Description != null)
+                {
+                    Dictionary<string, object> args = new Dictionary<string, object> {
+                         {"pTitle",proposal.Title},
+                         {"pDescription",proposal.Description}
+                    };
+                    success = DBHelper.CallNonQuery("spProposalUpdate", args);
+                    if (success == "1")
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(500, success);
+                    }
+                }
+                else
+                {
+                    success = "El titulo y la descripcion no pueden estar vacios";
+                }
+            }
+            catch
+            {
+            }
+            return StatusCode(500, success);
+        }
+
+    }
+}
---
+++ UbSocial/Models/Proposal.cs
@@ -0,0 +1,28 @@
+﻿using UbSocial.Models.Helpers;
+
+namespace UbSocial.Models
+{
+    public class Proposal
+    {
+        private string? _title;
+        private string? _description;
+
+        public Proposal() {
+        }
+
+        public string? Title { get => _title; set => _title = value; }
+        public string? Description { get => _description; set => _description = value; }
+
+        public string Create (Proposal proposal)
+        {            
+            
+            Dictionary<string, object> args = new Dictionary<string, object> {
+                    {"pTitle",proposal._title},
+                    {"pdescription",proposal._description},
+            };
+
+            return (DBHelper.CallNonQuery("spProposalCreate", args));           
+            
+        }
+    }
+}
