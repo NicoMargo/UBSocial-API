e83a610ea59ebd5693400e4fc1af89f92292328c
Nicolas Margossian <nmargo@pm.me>
2023-07-02 03:10:18 -0300

    Update DownloadableContentController.cs

--- UbSocial/Controllers/DownloadableContentController.cs
+++ UbSocial/Controllers/DownloadableContentController.cs
@@ -229,6 +229,15 @@ namespace UBSocial.Controllers
 
             try
             {
+                // Combina la ruta de la carpeta wwwroot con el nombre del archivo para obtener la ruta completa del archivo.
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "WWWRoot", URL);
+
+                // Verifica si el archivo existe
+                if (!System.IO.File.Exists(filePath))
+                {
+                    return NotFound();
+                }
+
                 Dictionary<string, object> args = new Dictionary<string, object> {
                     {"pId",idUser}
                 };
@@ -237,14 +246,7 @@ namespace UBSocial.Controllers
 
                 if (success == "True")
                 {
-                    // Combina la ruta de la carpeta wwwroot con el nombre del archivo para obtener la ruta completa del archivo.
-                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "WWWRoot", URL);
-
-                    // Verifica si el archivo existe
-                    if (!System.IO.File.Exists(filePath))
-                    {
-                        return NotFound();
-                    }
+                    
 
                     // Obtiene el tipo MIME del archivo (importante para saber cómo el navegador debe manejar la descarga)
                     var mimeType = GetMimeType(filePath);
